dc15a8c4f999cade91e6eabc3df2c0aa03516c3f
farzeen-alii <techzeen10@gmail.com>
2024-11-19 21:42:17 +0500

    Decorator & Standalone Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CounterComponent } from "./counter/counter.component";
+import { UserProfileComponent } from "./user-profile/user-profile.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CounterComponent],
+  imports: [RouterOutlet, CounterComponent, UserProfileComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
